4640d988ca0407ba4a8d0dfe4deb1483db254f8e
sm1996 <shamama.muhit@googlemail.com>
2019-04-03 11:30:23 +0100

    Added interaction tables

--- sqlcode/IGStructure.sql/IGStructure.sql/IGStructureWrite.sql
+++ sqlcode/IGStructure.sql/IGStructure.sql/IGStructureWrite.sql
@@ -65,3 +65,62 @@ hashtag varchar(2200) null
 )
 go
 
+--interaction tables
+
+create table interaction.dm
+(
+messageid bigint identity primary key,
+sender varchar(30) not null
+	constraint fk_sender foreign key(sender)
+	references userinfo.userprofile(handle),
+recipient varchar(30) not null
+	constraint fk_recipient foreign key(recipient)
+	references userinfo.userprofile(handle),
+timestamp datetime not null,
+postid bigint null
+	constraint fk_dm_postid foreign key(postid)
+	references post.info(postid),
+messagetext varchar(8000) null
+)
+go
+
+create table interaction.postview
+(
+activityid bigint identity primary key,
+viewer varchar(30) not null
+	constraint fk_viewer foreign key(viewer)
+	references userinfo.userprofile(handle),
+postid bigint not null
+	constraint fk_pv_postid foreign key(postid)
+	references post.info(postid),
+timestamp datetime not null,
+endviewtimestamp datetime not null
+)
+go
+
+create table interaction.postlike
+(
+activityid bigint identity primary key,
+liker varchar(30) not null
+	constraint fk_liker foreign key(liker)
+	references userinfo.userprofile(handle),
+postid bigint not null
+	constraint fk_pl_postid foreign key(postid)
+	references post.info(postid),
+timestamp datetime not null
+)
+go
+
+create table interaction.postcomment
+(
+activityid bigint identity primary key,
+commenter varchar(30) not null
+	constraint fk_commenter foreign key(commenter)
+	references userinfo.userprofile(handle),
+postid bigint not null
+	constraint fk_pc_postid foreign key(postid)
+	references post.info(postid),
+timestamp datetime not null
+)
+go
+
